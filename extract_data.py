c3975f9629ff9ff32e336ea78e8c81a566661f0a
Guy G <guy.griffiths@the-iea.org>
2018-10-05 10:07:11 +0100

    Added extracting a timeseries of an average average

--- extract_data.py
+++ extract_data.py
@@ -2,27 +2,21 @@
 """
 This module contains functions for extracting data from multifile NetCDF
 datasets, for use in conjunction with the TAMSAT alert system.
-
-It is currently in early stages and only contains a single function
-for extracting data at a point.  It will later contain methods for
-averaging over a bounding box, or potentially over a shapefile
-(so that averages for an entire country can be easily extracted)
 """
 
 import xarray as xr
 import pandas as pd
 from glob import glob
 
-def extract_point_timeseries(path, lon, lat):
+
+def _get_dataset(path):
     """
-    Extracts a timeseries from a set of NetCDF files at a specified location.
+    Creates an xarray dataset from a glob expression
 
     :param path: A glob expression specifying the location of the data.
                  When full paths are listed, the alphanumeric order of
                  the files must match the time order
-    :param lat: The latitude at which to extract a timeseries
-    :param lon: The longitude at which to extract a timeseries
-    :return: A pandas DataFrame containing all variables present in the NetCDF dataset
+    :return: An xarray dataset
     """
     # Construct list of files to use.  These should be alphanumerically ordered
     file_list = sorted(glob(path))
@@ -33,12 +27,58 @@ def extract_point_timeseries(path, lon, lat):
                                 autoclose=True)
 
     # Decode CF metadata (this is quick, but creates a new dataset)
-    dataset = xr.decode_cf(dataset)
+    return xr.decode_cf(dataset)
 
-    # Extract nearest neighbour to co-ordinate of interest, for all variables
+
+def extract_point_timeseries(path, lon, lat):
+    """
+    Extracts a timeseries from a set of NetCDF files at a specified location.
+
+    :param path: A glob expression specifying the location of the data.
+                 When full paths are listed, the alphanumeric order of
+                 the files must match the time order
+    :param lon: The longitude at which to extract a timeseries
+    :param lat: The latitude at which to extract a timeseries
+    :return: A pandas DataFrame containing all variables present in the NetCDF dataset
+    """
+    dataset = _get_dataset(path)
+
+    # Select nearest neighbour to co-ordinate of interest, for all variables
     timeseries = dataset.sel(lat=lat, lon=lon, method='nearest')
 
-    # Create a pandas DataFrame from the extracted data
+    # Create a pandas DataFrame from the selected data
+    # This is where the extraction happens
+    df = timeseries.to_dataframe()
+
+    return df
+
+
+def extract_area_mean_timeseries(path, minlon, maxlon, minlat, maxlat):
+    """
+    Extracts a timeseries from a set of NetCDF files averaged over a specified region.
+
+    :param path: A glob expression specifying the location of the data.
+                 When full paths are listed, the alphanumeric order of
+                 the files must match the time order
+    :param minlat: The minimum latitude of the region over which to extract a timeseries
+    :param maxlat: The maximum latitude of the region over which to extract a timeseries
+    :param minlon: The minimum longitude of the region over which to extract a timeseries
+    :param maxlon: The maximum longitude of the region over which to extract a timeseries
+    :return: A pandas DataFrame containing all variables present in the NetCDF dataset
+    """
+    dataset = _get_dataset(path)
+
+    ln = dataset.coords['lon']
+    lt = dataset.coords['lat']
+    # Select the region of the data
+    subset = dataset.loc[dict(
+        lon=ln[(ln >= minlon) & (ln <= maxlon)],
+        lat=lt[(lt >= minlat) & (lt <= maxlat)])]
+    # Take the mean over the lon/lat dimensions
+    timeseries = subset.mean(dim=('lon', 'lat'), skipna=True)
+
+    # Create a pandas DataFrame from the selected data
+    # This is where the extraction happens
     df = timeseries.to_dataframe()
 
     return df
